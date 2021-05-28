4f21a07d2111aa86092372942bab8a0a66c6a98b
mango <2859893460@qq.com>
2021-05-28 23:34:10 +0800

    feat: 添加拦截器获取响应数据

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -2,14 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
 import { logger } from './middleware/logger.middleware';
 import * as express from "express"
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
-  app.setGlobalPrefix("admin");
+
+  // 中间件
   // 解析数据
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(logger)
+
+  // 全局拦截器
+  app.useGlobalInterceptors(new TransformInterceptor())
+
+
+  app.setGlobalPrefix("admin");
   await app.listen(3000);
 }
 bootstrap();
