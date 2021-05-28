0371a1ac6b639094b2e29e053d1f524ce616c28d
张梓康 <zhangzikang>
2021-05-28 17:49:29 +0800

    feat: 添加日志打印

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
+import { logger,LoggerMiddleware } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
   app.setGlobalPrefix("admin");
+  app.use(LoggerMiddleware)
   await app.listen(3000);
 }
 bootstrap();
