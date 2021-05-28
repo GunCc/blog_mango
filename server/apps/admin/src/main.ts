69ce07bf87f5c31561633cf324111e8890400ce9
mango <2859893460@qq.com>
2021-05-28 23:26:27 +0800

    feat: 完成logger日志

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
-import { logger,LoggerMiddleware } from './middleware/logger.middleware';
+import { logger } from './middleware/logger.middleware';
+import * as express from "express"
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
   app.setGlobalPrefix("admin");
-  // app.use(new LoggerMiddleware())
+  // 解析数据
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(logger)
   await app.listen(3000);
 }
 bootstrap();
