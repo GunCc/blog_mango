9c6b5e8fc0bb02815bd4b847785e7dfa2728cdeb
张梓康 <zhangzikang>
2021-06-01 18:09:13 +0800

    feat: 搜索文章

--- server/apps/admin/src/logical/article/article.service.ts
+++ server/apps/admin/src/logical/article/article.service.ts
@@ -10,7 +10,16 @@ export class ArticleService {
 
         // 分页参数
         const { pageIndex = 1, pageSize, keywords = '' } = body;
-
+        const currentIndex = (pageIndex - 1) * pageSize < 0 ? 0 : (pageIndex - 1) * pageSize;
+        const queryArticleListSQL = `
+            SELECT 
+                id, type_id t_id, article_name name, article_desc, create_time, update_time
+            FORM 
+                m_blog_article
+            ORDER BY          
+                id DESC 
+            LIMIT ${currentIndex},${pageSize} 
+        `
 
     }
 
