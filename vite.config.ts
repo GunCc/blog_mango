a00f80d8912cff9fe52e39508adf4757d433857b
Mango <2859893460@qq.com>
2023-01-10 10:57:04 +0800

    feat:初始化项目、安装antd和less

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,36 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-
+import Components from "unplugin-vue-components/vite"
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
+import path from "node:path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: "less", // 一定要开启这个配置项
+        })
+      ]
+    })
+  ],
   resolve: {
     alias: {
+      'vue': 'vue/dist/vue.esm-bundler.js', // 定义vue的别名，如果使用其他的插件，可能会用到别名，该配置同时解决ant-design-vue中单页递归生成导航菜单时会出现异常的问题
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          hack: `true;@import (reference) "${path.resolve("src/design/index.less")}"`
+        },
+        javascriptEnabled: true
+      }
+    }
+  }
 });
